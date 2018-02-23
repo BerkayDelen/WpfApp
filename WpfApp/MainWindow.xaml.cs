e291709d58113f950e3ca5c3a8fd2d3324635d5c
AREL <AREL@B107-10>
2018-02-23 14:47:40 +0300

    Add Lİst to Data Grid

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Customer> customers;
         public MainWindow()
         {
             InitializeComponent();
+
+            
+            customers = new List<Customer>()
+            {
+                new Customer() { AD="Ali",SOYAD="CAN" },
+                 new Customer() { AD="Berkay",SOYAD="DELEN" },
+                 new Customer() { AD="Mert",SOYAD="KORKMAZ" }
+                
+            };
+
+            Customer cs = new Customer() { AD = "Hasan", SOYAD = "Ali" };
+            customers.Add(cs);
+            tb1.ItemsSource = customers;
+
         }
+
+        public void Windows_Load(object sender, RoutedEventArgs e)
+        {
+            
+            
+        }
+
+       
+    }
+
+    internal class Customer
+    {
+        public string AD { get; set; }
+        public string SOYAD { get; set; }
     }
 }
